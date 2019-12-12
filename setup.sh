142e6185d36c3018cdba26f700f1a737c83fea21
Jimmy <jimmy.mesta@gmail.com>
2019-12-12 14:29:49 -0800

    Update setup.sh
    quick prefix swap

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # Set env vars
-export DEVSHELL_PROJECT_ID="gke-${USER}"
-export GOOGLE_PROJECT_ID="gke-${USER}"
+export DEVSHELL_PROJECT_ID="nik-${USER}"
+export GOOGLE_PROJECT_ID="nik-${USER}"
 
 # Set the project in gcloud
-gcloud config set project "gke-${USER}"
+gcloud config set project "nik-${USER}"
 
 # Fetch the cluster location
 LOCATION="$(gcloud container clusters list --format='value(location)')"
 
 # Fetch a valid kubeconfig
-gcloud container clusters get-credentials --zone="${LOCATION}" "gke-${USER}"
+gcloud container clusters get-credentials --zone="${LOCATION}" "nik-${USER}"
 
 if ! [ -x "$(command -v nmap)" ]; then
   echo -n "Installing nmap..."
